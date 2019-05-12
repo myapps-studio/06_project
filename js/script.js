17ca0226474140f05859872892eb063ba80c6770
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-05-12 15:48:27 +0200

    random quates- finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Zadanie: Pierwsze zapytanie do serwera */
+
 var url = 'http://api.icndb.com/jokes/random';
 
 var button = document.getElementById('get-joke');
@@ -17,4 +19,51 @@ function getJoke() {
     xhr.send();
   }
 
-  
+/* Zadanie: Aplikacja losująca cytaty */
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
+/* get quote*/
+
+function getQuote() {
+    fetch(prefix + quoteUrl, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(createTweet);
+}
+
+/* create tweet */
+
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
